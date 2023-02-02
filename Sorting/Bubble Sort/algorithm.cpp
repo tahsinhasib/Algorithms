d8275399866cb439adb896afd041631983ba376e
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-02-02 22:25:48 +0600

    Update algorithm.cpp

--- Sorting/Bubble Sort/algorithm.cpp
+++ Sorting/Bubble Sort/algorithm.cpp
@@ -5,28 +5,34 @@ using namespace std;
 int main()
 {
     int arr[] = {11,3,6,8,15,1};
-    int size = sizeof(arr)/sizeof(int);
-    int i, j, k;
-    int temp;
+    int n = sizeof(arr)/sizeof(int);
 
-    cout << "\nUnsorted array: ";
-    for(i=0; i<size; i++){
-        cout << arr[i] << " ";
-    }
+
+    int i;
+    int j;
+    int temp;
+    bool swapped;
 
     // algorithm
-    for(i=0; i<size-1; i++){
-        for(j=0; j<size-1-i; j++){
+    for(i=0; i<n-1; i++){
+        swapped = false;
+        for(j=0; j<n-1-i; j++){
             if(arr[j] > arr[j+1]){               
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                swapped = true;
             }
+        }
+        if(!swapped){
+            break;
         }
     }
 
+
+
     cout << "\nSorted array: ";
-    for(i=0; i<size; i++){
+    for(i=0; i<n; i++){
         cout << arr[i] << " ";
     }
     
